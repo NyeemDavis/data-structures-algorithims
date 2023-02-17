b25692f60a5af4f3097207888a85ffb929c31358
Nyeem Davis <davisnyeem@gmail.com>
2023-02-17 03:51:59 -0500

    added cartesian peroduct algorithim

---
+++ Algorithims/cartesian-product.js
@@ -0,0 +1,16 @@
+// Given two finite non-empty sets, find their Cartesian product
+
+
+function cartesianProduct(a, b) {
+    let AxB = []
+    for(i = 0; i < a.length; i++) {
+        for(j = 0; j < b.length; j++) {
+         AxB.push([a[i], b[j]])
+        }
+    }
+    return AxB
+}
+
+// Time complexity: O(logn)
+
+console.log(cartesianProduct([10, 11], [12, 13, 14]))
