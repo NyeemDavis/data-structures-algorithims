d8d78bb496e4f23438f409962d6dc824cdbeff93
Nyeem Davis <davisnyeem@gmail.com>
2023-02-17 06:20:52 -0500

    beans

--- Algorithims/recursive-binary-search.js
+++ Algorithims/recursive-binary-search.js
@@ -1,7 +1,11 @@
+// Given an assorted array of 'n' elements and a target element 't', find the index of t in the array. Return -1 if the element is not found
+
+
 function recursiveBinarySearh(arr, target) {
  return search(arr, target, 0, arr.length - 1)
 }
 
+// Helper function 
 function search(arr, target, leftIndex, rightIndex) {
     if(leftIndex > rightIndex) {
         return -1
