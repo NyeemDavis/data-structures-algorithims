64797dd1d0e30b21dd01257806267b3978b90f0e
Nyeem Davis <davisnyeem@gmail.com>
2023-02-18 03:40:45 -0500

    added array

---
+++ Data Structures/array.js
@@ -0,0 +1,7 @@
+// Insert / remove from end O(1)
+// Insert / remove from beginning O(n)
+// Access O(1)
+// Search O(n)
+// Push / pop O(1)
+// Shift / unshift / concat / slice / splice O(n)
+// forEach / map / filter / reduce O(n)
